ada7744e1b457f2b7c1ad8f5dc1cb701f5f534f7
Doga Tolgay <dtolgay@nia-login06.scinet.local>
2025-03-26 12:29:48 -0400

    Changing the isrf artificially to see its effect on Nco

---
+++ interpolate/run_python.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --account=rrg-rbond-ac
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --time=05:00:00
+#SBATCH --job-name=isrfMultFactor
+#SBATCH --output=isrfMultFactor.out
+#SBATCH --error=isrfMultFactor.err
+
+# cd to working directory 
+cd $SLURM_SUBMIT_DIR
+
+module purge 
+ml python/3.11.5ls
+
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 line_emissions 2 &
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 temperature 2 &
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 abundance 2 &
+
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 line_emissions 5 &
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 temperature 5 &
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 abundance 5 &
+
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 line_emissions 10 &
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 temperature 10 &
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 abundance 10 &
+
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 line_emissions 0.5 &
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 temperature 0.5 &
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 abundance 0.5 &
+
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 line_emissions 0.1 &
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 temperature 0.1 &
+python -u debug_interpolating_for_gas_particles.py m12i_res7100_md zoom_in 0.0 abundance 0.1 &
+
+wait
